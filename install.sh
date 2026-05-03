9c53112051ee8d50a662098d7429a320d1a83778
Wjph <wjph2k10@gmail.com>
2026-05-03 21:44:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 
+sudo curl("https://cdn.akamai.steamstatic.com/client/installer/steam.deb")
 sudo apt install btop
 sudo apt install fastfetch
+
+#steam install
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf install steam -y
 
 sudo dnf install btop
 sudo dnf install fastfetch
